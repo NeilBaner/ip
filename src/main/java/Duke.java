a2a95a806bef8a7f5ce0cd8e5064b325a940c798
neilbaner <30567828+NeilBaner@users.noreply.github.com>
2020-08-15 11:34:59 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
+    public static void printHorizontalLine(){
+        System.out.println("____________________________________________________________");
+    }
+    public static void printHello(){
+        System.out.println("Hello! I'm Duke. ");
+        System.out.println("What can I do for you?");
+    }
+    public static void printGoodBye(){
+        System.out.println("Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printHorizontalLine();
+        printHello();
+        printHorizontalLine();
+        printGoodBye();
+        printHorizontalLine();
     }
 }
