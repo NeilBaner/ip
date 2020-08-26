8fe1e1aba732aa0094a50813479f44abbd89ac7d
neilbaner <30567828+NeilBaner@users.noreply.github.com>
2020-08-26 21:47:14 +0800

    Implement Add, List
    Add the ability to add tasks and view a list of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,30 +1,48 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static TaskList list = new TaskList();
+    
     public static void printHorizontalLine(){
         System.out.println("____________________________________________________________");
     }
     public static void printHello(){
+        printHorizontalLine();
         System.out.println("Hello! I'm Duke. ");
         System.out.println("What can I do for you?");
+        printHorizontalLine();
     }
     public static void printGoodBye(){
         System.out.println("Bye. Hope to see you again soon!");
     }
+    
+    public static void printTaskList(){
+        for (int i = 0; i < list.getSize(); i++) {
+            Task t = list.getTask(i);
+            System.out.println((i + 1) + ". " + t.getTitle());
+        }
+    }
+
+    public static void addTask(String title){
+        list.addTask(new Task(title));
+    }
+
     public static void main(String[] args) {
         Scanner k = new Scanner(System.in);
         String input;
-        printHorizontalLine();
         printHello();
-        printHorizontalLine();
         while(true){
             input = k.nextLine();
             if(input.toLowerCase().equals("bye")){
                 break;
+            } else if(input.toLowerCase().equals("list")){
+                printTaskList();
+                continue;
+            }else {
+                addTask(input);
+                System.out.println("Added task: " + input);
+                continue;
             }
-            printHorizontalLine();
-            System.out.println(input);
-            printHorizontalLine();
         }
         printHorizontalLine();
         printGoodBye();
