1508dfaea3f54e18b8dcfcd7dd6af158f7502066
neilbaner <30567828+NeilBaner@users.noreply.github.com>
2020-08-30 15:43:54 +0800

    Add Messages class
    Added a Messages class to print messages, implementing SLAP to some extent.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,72 +3,49 @@ import java.util.Scanner;
 public class Duke {
     public static TaskList list = new TaskList();
 
-    public static void printHorizontalLine() {
-        System.out.println("____________________________________________________________");
-    }
 
-    public static void printHello() {
-        printHorizontalLine();
-        System.out.println("Hello! I'm Duke. ");
-        System.out.println("What can I do for you?");
-        printHorizontalLine();
-    }
-
-    public static void printGoodBye() {
-        System.out.println("Bye. Hope to see you again soon!");
-    }
 
     public static void printTaskList() {
-        printHorizontalLine();
+        Messages.printHorizontalLine();
         if (list.getSize() == 0) {
             System.out.println("No tasks added yet");
         }
         for (Task task : list.getAllTasks()) {
             task.print();
         }
-        printHorizontalLine();
+        Messages.printHorizontalLine();
     }
 
     public static void addToDo(String title) {
         list.addTask(new ToDo(title));
-        printHorizontalLine();
-        System.out.println("Done. I've added the following to-do to your list: " + title);
-        printHorizontalLine();
+        Messages.printAddedToDo(title);
     }
 
     public static void addEvent(String title, String eventTime) {
         list.addTask(new Event(title, eventTime));
-        printHorizontalLine();
-        System.out.println("Roger dodger chief. I've added this event to your list: " + title);
-        printHorizontalLine();
+        Messages.printAddedEvent(title);
     }
 
     public static void addDeadline(String title, String dueDate) {
         list.addTask(new Deadline(title, dueDate));
-        printHorizontalLine();
-        System.out.println("You got it. Added a new deadline to your list, titled : " + title);
-        printHorizontalLine();
+        Messages.printAddedDeadline(title);
     }
 
     public static void markDone(int indexToMark) throws NumberFormatException, IndexOutOfBoundsException {
         list.markTaskDone(indexToMark);
-        printHorizontalLine();
-        System.out.println(list.getTask(indexToMark).getTitle() + " marked as done.");
-        printHorizontalLine();
+        Messages.printMarkedDone(list.getTask(indexToMark).getTitle());
     }
 
     public static void main(String[] args) {
         Scanner k = new Scanner(System.in);
         String input;
         boolean toContinue = true;
-        printHello();
+        Messages.printHello();
         while (toContinue) {
             input = k.nextLine();
             if (input.toLowerCase().equals("bye")) {
                 toContinue = false;
-                printHorizontalLine();
-                printGoodBye();
-                printHorizontalLine();
+                Messages.printGoodBye();
             } else if (input.toLowerCase().equals("list")) {
                 printTaskList();
             } else if (input.toLowerCase().startsWith("done")) {
@@ -76,11 +53,9 @@ public class Duke {
                     int indexToMark = Integer.parseInt(input.substring(5)) - 1;
                     markDone(indexToMark);
                 } catch (NumberFormatException e) {
-                    System.out.println("Incorrectly formatted input");
-                    printHorizontalLine();
+                    Messages.printFormattingError();
                 } catch (IndexOutOfBoundsException e) {
-                    System.out.println("Task does not exist in list");
-                    printHorizontalLine();
+                    Messages.printNonExistError();
                 }
             } else if (input.toLowerCase().startsWith("todo")) {
                 addToDo(input.substring(5));
@@ -90,7 +65,7 @@ public class Duke {
                     String dueDate = input.substring(input.indexOf("/by") + 4).strip();
                     addDeadline(title, dueDate);
                 } catch (IndexOutOfBoundsException e) {
-                    System.out.println("Incorrectly formatted input");
+                    Messages.printFormattingError();
                 }
             } else if (input.startsWith("event")) {
                 try {
@@ -98,7 +73,7 @@ public class Duke {
                     String eventTime = input.substring(input.indexOf("/at") + 4).strip();
                     addEvent(title, eventTime);
                 } catch (IndexOutOfBoundsException e) {
-                    System.out.println("Incorrectly formatted input");
+                    Messages.printFormattingError();
                 }
             }
         }
---
+++ src/main/java/Messages.java
@@ -0,0 +1,53 @@
+public class Messages {
+    public static void printHorizontalLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void printHello() {
+        printHorizontalLine();
+        System.out.println("Hello! I'm Duke. ");
+        System.out.println("What can I do for you?");
+        printHorizontalLine();
+    }
+
+    public static void printGoodBye() {
+        printHorizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Shutting down...");
+        printHorizontalLine();
+    }
+
+    public static void printAddedToDo(String title) {
+        printHorizontalLine();
+        System.out.println("Done. I've added the following to-do to your list: " + title);
+        printHorizontalLine();
+    }
+
+    public static void printAddedEvent(String title) {
+        Messages.printHorizontalLine();
+        System.out.println("Roger dodger chief. I've added this event to your list: " + title);
+        Messages.printHorizontalLine();
+    }
+
+    public static void printAddedDeadline(String title) {
+        Messages.printHorizontalLine();
+        System.out.println("You got it. Added a new deadline to your list, titled : " + title);
+        Messages.printHorizontalLine();
+    }
+
+    public static void printMarkedDone(String title) {
+        Messages.printHorizontalLine();
+        System.out.println(title + " marked as done.");
+        Messages.printHorizontalLine();
+    }
+
+    public static void printFormattingError() {
+        System.out.println("Incorrectly formatted input");
+        Messages.printHorizontalLine();
+    }
+
+    public static void printNonExistError() {
+        System.out.println("Task does not exist in list");
+        Messages.printHorizontalLine();
+    }
+}
