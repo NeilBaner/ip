8e41fd4b86767de9e6678eb0f4ad3162c11a3bdf
neilbaner <30567828+NeilBaner@users.noreply.github.com>
2020-09-04 15:16:25 +0800

    Refactor input parsing methods
    Refactor the complex methods to parse the input string into separate methods to improve readability.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -35,6 +35,64 @@ public class Duke {
         Messages.printMarkedDone(list.getTask(indexToMark).getTitle());
     }
 
+    private static String getEventTime(String input) {
+        return input.substring(input.indexOf("/at") + 4).strip();
+    }
+
+    private static String getEventTitle(String input) {
+        return input.substring(6, input.indexOf("/at")).strip();
+    }
+
+    private static String getDeadlineDueDate(String input) {
+        return input.substring(input.indexOf("/by") + 4).strip();
+    }
+
+    private static int getIndexToMark(String input) {
+        return Integer.parseInt(input.substring(5)) - 1;
+    }
+
+    private static String getDeadlineTitle(String input) {
+        return input.substring(9, input.indexOf("/by")).strip();
+    }
+
+    private static boolean parseInput(String input) {
+        boolean toContinue = true;
+        if (input.toLowerCase().equals("bye")) {
+            toContinue = false;
+            Messages.printGoodBye();
+        } else if (input.toLowerCase().equals("list")) {
+            printTaskList();
+        } else if (input.toLowerCase().startsWith("done")) {
+            try {
+                int indexToMark = getIndexToMark(input);
+                markDone(indexToMark);
+            } catch (NumberFormatException e) {
+                Messages.printFormattingError();
+            } catch (IndexOutOfBoundsException e) {
+                Messages.printNonExistError();
+            }
+        } else if (input.toLowerCase().startsWith("todo")) {
+            addToDo(input.substring(5));
+        } else if (input.toLowerCase().startsWith("deadline")) {
+            try {
+                String title = getDeadlineTitle(input);
+                String dueDate = getDeadlineDueDate(input);
+                addDeadline(title, dueDate);
+            } catch (IndexOutOfBoundsException e) {
+                Messages.printFormattingError();
+            }
+        } else if (input.startsWith("event")) {
+            try {
+                String title = getEventTitle(input);
+                String eventTime = getEventTime(input);
+                addEvent(title, eventTime);
+            } catch (IndexOutOfBoundsException e) {
+                Messages.printFormattingError();
+            }
+        }
+        return toContinue;
+    }
+
     public static void main(String[] args) {
         Scanner k = new Scanner(System.in);
         String input;
@@ -42,39 +100,7 @@ public class Duke {
         Messages.printHello();
         while (toContinue) {
             input = k.nextLine();
-            if (input.toLowerCase().equals("bye")) {
-                toContinue = false;
-                Messages.printGoodBye();
-            } else if (input.toLowerCase().equals("list")) {
-                printTaskList();
-            } else if (input.toLowerCase().startsWith("done")) {
-                try {
-                    int indexToMark = Integer.parseInt(input.substring(5)) - 1;
-                    markDone(indexToMark);
-                } catch (NumberFormatException e) {
-                    Messages.printFormattingError();
-                } catch (IndexOutOfBoundsException e) {
-                    Messages.printNonExistError();
-                }
-            } else if (input.toLowerCase().startsWith("todo")) {
-                addToDo(input.substring(5));
-            } else if (input.toLowerCase().startsWith("deadline")) {
-                try {
-                    String title = input.substring(9, input.indexOf("/by")).strip();
-                    String dueDate = input.substring(input.indexOf("/by") + 4).strip();
-                    addDeadline(title, dueDate);
-                } catch (IndexOutOfBoundsException e) {
-                    Messages.printFormattingError();
-                }
-            } else if (input.startsWith("event")) {
-                try {
-                    String title = input.substring(6, input.indexOf("/at")).strip();
-                    String eventTime = input.substring(input.indexOf("/at") + 4).strip();
-                    addEvent(title, eventTime);
-                } catch (IndexOutOfBoundsException e) {
-                    Messages.printFormattingError();
-                }
-            }
+            toContinue = parseInput(input);
         }
     }
 }
