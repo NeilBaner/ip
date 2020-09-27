00ed450a4be0e3db8080b7238dc6c9225a143720
neilbaner <30567828+NeilBaner@users.noreply.github.com>
2020-09-27 15:54:39 +0800

    Fix formatting

--- src/main/java/com/neilbaner/duke/exceptions/BadDateFormatException.java
+++ src/main/java/com/neilbaner/duke/exceptions/BadDateFormatException.java
@@ -3,6 +3,6 @@ package com.neilbaner.duke.exceptions;
 public class BadDateFormatException extends DukeException {
     @Override
     public void printErrorMessage(String command) {
-        
+
     }
 }
