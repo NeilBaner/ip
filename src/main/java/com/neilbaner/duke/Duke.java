ea23cd904680ed244769e43eff2436e4aa61c811
neilbaner <30567828+NeilBaner@users.noreply.github.com>
2020-09-29 18:49:07 +0800

    Update main class to take advantage of all this OOP

--- src/main/java/com/neilbaner/duke/Duke.java
+++ src/main/java/com/neilbaner/duke/Duke.java
@@ -1,66 +1,38 @@
 package com.neilbaner.duke;
 
-import com.neilbaner.duke.exceptions.*;
+import com.neilbaner.duke.exceptions.DukeException;
 import com.neilbaner.duke.files.Storage;
-import com.neilbaner.duke.messages.Messages;
-import com.neilbaner.duke.task.*;
-import com.neilbaner.duke.ui.Commands;
-import com.neilbaner.duke.ui.InputParser;
+import com.neilbaner.duke.task.TaskList;
+import com.neilbaner.duke.ui.UserInterface;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Scanner;
+
+import static com.neilbaner.duke.ui.Commands.EXIT_COMMAND;
 
 public class Duke {
     public static TaskList list = new TaskList();
 
     public static final String SAVE_FILE_PATH = "dukesave.txt";
 
-    public static void addToDo(String title) {
-        list.addTask(new ToDo(title));
-        Messages.printAddedToDo(title);
-    }
-
-    public static void addEvent(String title, String eventTime) {
-        list.addTask(new Event(title, eventTime));
-        Messages.printAddedEvent(title);
-    }
-
-    public static void addDeadline(String title, String dueDate) {
-        list.addTask(new Deadline(title, dueDate));
-        Messages.printAddedDeadline(title);
-    }
-
-    public static void markAsDone(int indexToMark) throws NumberFormatException, IndexOutOfBoundsException {
-        list.markTaskDone(indexToMark);
-        Messages.printMarkedDone(list.getTask(indexToMark).getTitle());
-    }
-
-    public static void deleteTask(int indexToDelete) {
-        String deletedTitle = list.getTask(indexToDelete).toString();
-        list.deleteTask(indexToDelete);
-        Messages.printDeleted(deletedTitle);
-    }
-
     public static void main(String[] args) {
+        String input = "";
         try {
             Storage storage = new Storage(SAVE_FILE_PATH);
-            storage.loadState(list);
+            UserInterface ui = new UserInterface(list);
             Scanner k = new Scanner(System.in);
-            String input;
-            boolean toContinue = true;
-            Messages.printHello();
-            while (toContinue) {
+            storage.loadState(list);
+            ui.startUI();
+            while (!input.equals(EXIT_COMMAND)) {
                 input = k.nextLine();
                 try {
-                    toContinue = parseInput(input);
-                } catch (DukeException e) {
+                    ui.executeCommand(input);
+                } catch(DukeException e) {
                     e.printErrorMessage(input);
                 }
             }
             storage.saveState(list);
         } catch (DukeException e) {
-            e.printErrorMessage("");
+            e.printErrorMessage(input);
         }
     }
 }
