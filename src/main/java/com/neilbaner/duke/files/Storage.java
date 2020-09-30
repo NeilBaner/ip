3bd260d70462c79f96e80001fe8d2ae82b266148
neilbaner <30567828+NeilBaner@users.noreply.github.com>
2020-09-30 21:37:27 +0800

    Add javadoc comments to Storage class.

--- src/main/java/com/neilbaner/duke/files/Storage.java
+++ src/main/java/com/neilbaner/duke/files/Storage.java
@@ -13,9 +13,20 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * This class is used for file storage operations to save and load Duke's state to and from a text file stored on the
+ * disk.
+ */
 public class Storage {
     private final String filePath;
 
+    /**
+     * Load the state of Duke that was previously saved onto a file on the disk.
+     * @param list the <code>TaskList</code> to which to copy the tasks read from the file.
+     * @throws NoSaveFileException if the save file could not be found on the disk.
+     * @throws DeserializerException if there is some error deserializing the text file, should only occur if the
+     * save is corrupted or tampered with in some way.
+     */
     public void loadState(TaskList list) throws NoSaveFileException, DeserializerException {
         File saveFile = new File(filePath);
         try {
@@ -31,6 +42,11 @@ public class Storage {
         }
     }
 
+    /**
+     * Save the state of Duke to a file on the disk.
+     * @param list the <code>TaskList</code> from which to read the tasks to write to the file on the disk.
+     * @throws FileSavingException if there is some error writing the file to the disk.
+     */
     public void saveState(TaskList list) throws FileSavingException{
         try {
             FileWriter fw = new FileWriter(filePath, false);
